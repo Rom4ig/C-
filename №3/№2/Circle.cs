b026318499af9a596b305ffd01b353f84c4cc681
Rom4ig <43033763+Rom4ig@users.noreply.github.com>
2019-02-28 20:23:49 +0300

    Add files via upload

---
+++ №3/№2/Circle.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Circle
+    {
+        readonly int id;
+        private double r;
+        public double Ox, Oy;
+        static int count = 0;
+        const double PI = Math.PI;
+        public double R
+        {
+            set
+            {
+                if (value < 0)
+                { Console.Write("Радиус должен быть положительным: "); r = Convert.ToDouble(Console.ReadLine()); }
+                else { r = value; }
+            }
+            get { return r; }
+        }
+        //Конструкторы
+        public Circle()
+        {
+            Ox = 2;
+            Oy = 3;
+            R = 6;
+            count++;
+        }
+        public Circle(double r)
+        {
+            R = r; Ox = 14; Oy = 13; count++;
+        }
+        public Circle(double Ox, double Oy, double R)
+        {
+            this.Ox = Ox;
+            this.Oy = Oy;
+            this.R = R;
+            count++;
+
+        }
+        static Circle()
+        {
+            Console.WriteLine("Статический конструктор");
+        }
+        public virtual Boolean Equals(Circle a)
+        {
+            if (a == null) return false;
+            if (this.GetType() != a.GetType()) return false;
+            if (this.Ox != a.Ox) return false;
+            if (this.Oy != a.Oy) return false;
+            if (this.R != a.R) return false;
+            return true;
+        }
+        public override int GetHashCode()
+        {
+            int hashcode = this.id.GetHashCode();
+            hashcode = 7 * hashcode + this.Ox.GetHashCode();
+            hashcode = 7 * hashcode + this.Oy.GetHashCode();
+            hashcode = 7 * hashcode + this.R.GetHashCode();
+            return hashcode;
+        }
+        private Circle(double x, double y)
+        {
+            R = 1; Ox = x; Oy = y;
+            count++;
+        }
+        public override string ToString()
+        {
+            return "Информация о окружности: \nКоординаты центра:" + this.Ox + "," + this.Oy + " Радиус окружности: " + this.R;
+        }
+        //public void FindCenter(Circle[] a)
+        //{
+        //    for (int i = 0; i < a.Length; i++)
+        //    {
+        //        for (int j = i + 1; j < a.Length; j++)
+        //        {
+        //            for (int k = i + 2; k < a.Length; k++)
+        //            {
+        //                if (((a[i].Oy - a[j].Oy) / (a[k].Oy - a[j].Oy)) == ((a[i].Ox - a[j].Ox) / (a[k].Ox - a[j].Ox)))
+        //                {
+        //                    Console.WriteLine("\nОкружности  {0} {1} {2} лежат на одной прямой", i + 1, j + 1, k + 1);
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+        public double Square()
+        {
+            return PI * this.R * this.R;
+
+        }
+        public double line()
+        { return Math.PI * 2 * this.R; }
+        public static void DisplayCount()
+        {
+            Console.WriteLine($"Создано {count} объектов класса Circle");
+        }
+        public void SRORIFM(ref double Ox, ref double Oy, ref double R, out int srorifm)
+        {
+            srorifm = (int)(Ox + Oy + R) / 2;
+ 
+        }
+    }
+}
---
+++ №3/№2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle[] array = new Circle[]
+ {
+            new Circle(11, 10, -5),
+            new Circle(13, 12, 14),
+            new Circle(12, 11, 17),
+            new Circle(11, 11, 14),
+            new Circle(11, 1, 14),
+            new Circle(14),
+            new Circle(),
+
+ };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("{0} - {1}", i + 1, array[i].ToString());
+                Console.WriteLine("Hachcode {0} - {1}\n\n", i + 1, array[i].GetHashCode());
+            }
+            Console.WriteLine(array[1].GetType());
+            Console.WriteLine(array[1].Equals(array[0]));
+            Circle.DisplayCount();
+            double max = System.Double.MinValue;
+            double min = System.Double.MaxValue;
+            int i_max = 0;
+            int i_min = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Square() > max ) { max = array[i].Square(); i_max = i; }
+                if (array[i].Square() < min) { min = array[i].Square(); i_min = i; }
+            }
+            Console.WriteLine("Наибольшая площадь у окружности №{0} и S={1}", i_max + 1, max);
+            Console.WriteLine("Наименьшая площадь у окружности №{0} и S={1}", i_min + 1, min);
+             max = System.Double.MinValue;
+             min = System.Double.MaxValue;
+             i_max = 0;
+             i_min = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].line() > max) { max = array[i].line(); i_max = i; }
+                if (array[i].line() < min) { min = array[i].line(); i_min = i; }
+            }
+            Console.WriteLine("Наибольшая длина окружности №{0} и L={1}", i_max + 1, max);
+            Console.WriteLine("Наименьшая длина окружности №{0} и L={1}", i_min + 1, min);
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    for (int k = j + 1; k < array.Length; k++)
+                    {
+                        if (((array[i].Oy - array[j].Oy) / (array[k].Oy - array[j].Oy)) == ((array[i].Ox - array[j].Ox) / (array[k].Ox - array[j].Ox))
+                            ||(array[i].Oy==array[j].Oy&& array[j].Oy == array[k].Oy)
+                            || (array[i].Ox == array[j].Ox && array[j].Ox == array[k].Ox))
+                        {
+                            Console.WriteLine("\nОкружности  {0} {1} {2} лежат на одной прямой", i + 1, j + 1, k + 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
